becdb2136df311f4c051b8b507c5bc2388537509
Preshh0 <uniquep201@gmail.com>
2022-01-18 23:47:38 +0100

    added test cases

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #third_party,
-    "rest_framework",
-    
+    'rest_framework',
+    'corsheaders',
+
     #local
     "todos",
 ]
@@ -48,12 +49,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', #appropriate location, above the "commonMiddleware"
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000', #REACT PORT
+    'http://localhost:8000', #DJANGO PORT
+)
 
 ROOT_URLCONF = 'config.urls'
 
